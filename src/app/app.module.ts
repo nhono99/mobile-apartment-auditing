2a095726b6f9daec1cfc140a2a1b98b2b273e068
ronald <nhonoskywalker@yahoo.com>
2020-01-09 14:33:59 +0800

    firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,28 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDcwUCNoeToQ2g-iOzcyJigch_a5Xy88aU',
+  authDomain: 'apartment-auditing-c9475.firebaseapp.com',
+  databaseURL: 'https://apartment-auditing-c9475.firebaseio.com',
+  projectId: 'apartment-auditing-c9475',
+  storageBucket: 'apartment-auditing-c9475.appspot.com',
+  messagingSenderId: '831580791351',
+  appId: '1:831580791351:web:d54148e86294c6381215b2'
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [
     StatusBar,
@@ -24,4 +39,4 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
